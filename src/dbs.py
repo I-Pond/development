4e5e838b15260de2db0877724d7c1dada4778050
retnofaipurwanto@gmail.com <retnofaipurwanto@gmail.com>
2023-06-26 19:48:57 +0700

    terbaruuuuu bangettttt niccchhhhh

---
+++ src/dbs.py
@@ -0,0 +1,49 @@
+config = {
+  'apiKey': "AIzaSyBkTUbRY3HlLYqr43k0AX3z90xfXVCrb14",
+  'authDomain': "myipond-c4da9.firebaseapp.com",
+  'databaseURL': "https://myipond-c4da9-default-rtdb.asia-southeast1.firebasedatabase.app",
+  'projectId': "myipond-c4da9",
+  'storageBucket': "myipond-c4da9.appspot.com",
+  'messagingSenderId': "890230681173",
+  'appId': "1:890230681173:web:dee99b3fc85da7e2c3aedc",
+  'measurementId': "G-VH4CWG4LT2"
+}
+db_pos = {
+    'DB_NAME' : 'ipond',
+    'user' : 'postgres',
+    'password' : 'adamdvimprez7',
+    'host':'localhost',
+    'port': 5432
+}
+db_postgre = { 
+    'DB_NAME':'wst', 
+    'user' : 'postgres',
+    'password' : 'adamdvimprez7',
+    'host':'localhost',
+    'port': 5432
+}
+db_streamlit = { 
+    'DB_NAME':'weather_station', 
+    'user' : 'postgres',
+    'password' : 'adamdvimprez7',
+    'host':'localhost',
+    'port': 5432
+}
+config_dt_streamlit = {
+  'apiKey': "AIzaSyAx3KfMijz3Grt0o3z9t7_AjlGVT432LRU",
+  'authDomain': "datastreamlit.firebaseapp.com",
+  'databaseURL': 'https://datastreamlit-default-rtdb.asia-southeast1.firebasedatabase.app/',
+  'projectId': "datastreamlit",
+  'storageBucket': "datastreamlit.appspot.com",
+  'messagingSenderId': "204827362978",
+  'appId': "1:204827362978:web:db4d71f5793668c21039fb"
+}
+config23 = { 
+    'apiKey': "AIzaSyDHSkBeH9x1Eq9tB0sFTsdadQ0kn-QHf08",
+    'authDomain': "ipond1.firebaseapp.com",
+    'databaseURL': "https://ipond1-default-rtdb.asia-southeast1.firebasedatabase.app",
+    'projectId': "ipond1",
+    'storageBucket': "ipond1.appspot.com",
+    'messagingSenderId': "1079573392117",
+    'appId': "1:1079573392117:web:b0e0944f2e4433d99933bc"
+}
---
+++ str.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+import joblib
+import pyrebase
+from src.dbs import config_dt_streamlit
+from datetime import datetime
+
+# load the pre-trained model and scaler
+model = joblib.load('modelss5.pkl')
+
+def predict_weather(turbinity,temperature,ph):
+    # create a dataframe with the user input
+    input_data = pd.DataFrame([[turbinity,temperature,ph]], 
+                              columns=['ph','temperature','turbinity'])
+    # make predictions
+    prediction = model.predict(input_data)
+    return prediction[0]
+
+firebase = pyrebase.initialize_app(config_dt_streamlit)
+db = firebase.database()
+# current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S" )
+current_date = datetime.now()
+waktu_sekrng = current_date.strftime("%H:%M:%S")
+
+st.title('Water Quality Sensor For Nila Fisherman')
+st.header('Input hasil dari data IoT untuk dapat memberikan hasil kekeruhan: ')
+turbinity = st.number_input('Masukin Tingkat Kekeruhan:')
+ph = st.number_input('Masukin Ph:')
+temperature = st.number_input('Masukin Temperature:')
+
+if st.button('Predict'):
+    prediction = predict_weather(turbinity,temperature,ph)
+    if prediction == 0:
+        st.write('Bersih')
+    else: 
+        st.write('Keruh')
+    data = {
+        'ph': float(ph),
+        'temperature':float(temperature),
+        'kekeruhan':float(turbinity),
+        'prediksi': int(prediction),
+        'tanggal': current_date.date().isoformat(), 
+        'waktu': waktu_sekrng
+    }
+    db.child('prediksi_kualitas_air').push(data)
+
+    st.success('Prediksi berhasil didistribusikan ke Firebase.')
