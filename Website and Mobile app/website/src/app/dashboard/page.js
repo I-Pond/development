65a8f1ee5087582ec234b8c127b4e005e8854859
Ahda Akmalul Ilmi <ahda043+GitHub@gmail.com>
2023-08-31 20:12:20 +0700

    Updating dashboard page

--- Website and Mobile app/website/src/app/dashboard/page.js
+++ Website and Mobile app/website/src/app/dashboard/page.js
@@ -3,11 +3,11 @@ import style from "@/app/dashboard/dashboard.module.css";
 export default function Dashboard() {
   return (
     <>
-      <div className={`style.container`}>
-        <h1>Dashboard</h1>
+      <div className={`${style.container}`}>
+        <h1 className={`${style.title}`}>Dashboard</h1>
         <iframe
-          width="600"
-          height="1350"
+          width="100%"
+          height="80%"
           src="https://lookerstudio.google.com/embed/reporting/c35bb677-d509-4f04-b9be-dcc5f4334cf2/page/SQGbD"
           frameborder="0"
           style={{ border: 0 }}
